0b7b24071fc951aca673edfbc440d7c735542da1
yozu <yasuo@ozu.email>
2022-07-16 23:20:43 +0900

    Move everything into bundler

--- bundler/build.rs
+++ bundler/build.rs
@@ -2,25 +2,30 @@ use std::{env, process::Command};
 
 fn main() {
 	let out_dir = env::var("OUT_DIR").unwrap();
-	let satysfi_dir = "./contrib/satysfi";
+	let switch_name = format!("{}/", out_dir);
+	let satysfi_dir = "../ocaml/satysfi";
 	Command::new("rm")
-		.args(&["-rf", &format!("{}/_opam", satysfi_dir)])
+		.args(&[
+			"-rf",
+			&format!("{}/libsatysfi.a", out_dir),
+			&format!("{}/libsatysfi.o", out_dir),
+			&format!("{}/_opam", out_dir),
+		])
 		.status()
-		.expect("rm _opam failed");
+		.expect("rm failed");
 	Command::new("opam")
-		.current_dir(satysfi_dir)
-		.args(&["switch", "create", ".", "--no-install"])
+		.args(&["switch", "create", &switch_name, "--no-install"])
 		.status()
 		.expect("Cannot create switch");
 	Command::new("opam")
-		.current_dir(satysfi_dir)
-		.args(&["install", "dune"])
+		.args(&["install", "--switch", &switch_name, "dune"])
 		.status()
 		.expect("Cannot install dune");
 	Command::new("opam")
-		.current_dir(satysfi_dir)
 		.args(&[
 			"repository",
+			"--switch",
+			&switch_name,
 			"add",
 			"satysfi-external",
 			"https://github.com/gfngfn/satysfi-external-repo.git",
@@ -28,9 +33,10 @@ fn main() {
 		.status()
 		.expect("Cannot register satysfi-external repo");
 	Command::new("opam")
-		.current_dir(satysfi_dir)
 		.args(&[
 			"repository",
+			"--switch",
+			&switch_name,
 			"add",
 			"satyrographos-repo",
 			"https://github.com/na4zagin3/satyrographos-repo.git",
@@ -38,31 +44,31 @@ fn main() {
 		.status()
 		.expect("Cannot register satyrographos-repo");
 	Command::new("opam")
-		.current_dir(satysfi_dir)
-		.args(&["update"])
+		.args(&["update", "--switch", &switch_name])
 		.status()
 		.expect("Cannot run opam update");
 	Command::new("opam")
 		.current_dir(satysfi_dir)
-		.args(&["pin", ".", "-y"])
+		.args(&["pin", "--switch", &switch_name, ".", "-y"])
 		.status()
 		.expect("Cannot run opam pin");
 	Command::new("opam")
 		.current_dir(satysfi_dir)
-		.args(&["exec", "--", "dune", "build", "src/main.a"])
-		.status()
-		.expect("Cannot run dune build");
-	Command::new("rm")
 		.args(&[
-			"-f",
-			&format!("{}/libsatysfi.a", out_dir),
-			&format!("{}/libsatysfi.o", out_dir),
+			"exec",
+			"--switch",
+			&switch_name,
+			"--",
+			"dune",
+			"build",
+			&format!("--build-dir={}/_build", out_dir),
+			"src/main.a",
 		])
 		.status()
-		.expect("rm failed");
+		.expect("Cannot run dune build");
 	Command::new("cp")
 		.args(&[
-			&format!("{}/_build/default/src/main.a", satysfi_dir),
+			&format!("{}/_build/default/src/main.a", out_dir),
 			&format!("{}/libsatysfi.a", out_dir),
 		])
 		.status()
